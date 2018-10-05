82da46f5b8697c0dcc7d322352955d3f1521b031
Tungsteno74 <tungsteno74@yahoo.it>
2018-10-05 04:08:44 +0200

    fix python3 compatibility

--- test/event_detect.py
+++ test/event_detect.py
@@ -6,7 +6,7 @@ from threading import Timer
 SWITCH_PIN = 10
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(SWITCH_PIN,GPIO.IN,GPIO.PUD_DOWN)
-print "\n value_%d = %d\n" %(SWITCH_PIN,GPIO.input(SWITCH_PIN))
+print("\n value_%d = %d\n" %(SWITCH_PIN,GPIO.input(SWITCH_PIN)))
 
 GPIO.add_event_detect(SWITCH_PIN, GPIO.RISING,bouncetime=200)  # add rising edge detection on a channel
 
@@ -14,8 +14,8 @@ switchcount = 0
 while switchcount < 2:   
 	if GPIO.event_detected(SWITCH_PIN):
 		switchcount += 1
-		print 'Button pressed',switchcount
-		print "\n value_%d = %d\n" %(SWITCH_PIN,GPIO.input(SWITCH_PIN))
+		print('Button pressed',switchcount)
+		print("\n value_%d = %d\n" %(SWITCH_PIN,GPIO.input(SWITCH_PIN)))
 		
 
 GPIO.remove_event_detect(SWITCH_PIN)
