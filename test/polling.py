3784119138344316bc842f55d5febe884dbb3d67
Tungsteno74 <tungsteno74@yahoo.it>
2018-10-05 04:14:28 +0200

    fix module name and python3 compatibility

--- test/polling.py
+++ test/polling.py
@@ -1,18 +1,18 @@
-import RPi.GPIO as GPIO
+import NPi.GPIO as GPIO
 import time
 
 channel = 7
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(channel,GPIO.IN,GPIO.PUD_DOWN)
-print " Now input is Low\n Our task is performed when it becomes High"
+print(" Now input is Low\n Our task is performed when it becomes High")
 
 while True:
     if GPIO.input(channel):
-        print "Input was High,begin to perform"
-	print "Count Down"
+        print("Input was High,begin to perform")
+	print("Count Down")
         for i in range(7,0,-1):
-            print "%d" %i
+            print("%d" %i)
             time.sleep(1)
-        print "Performed!"
+        print("Performed!")
 	exit()
