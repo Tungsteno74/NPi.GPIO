42db987f5f0f69bc6c450961e06a89ef876ae047
Tungsteno74 <tungsteno74@yahoo.it>
2018-10-05 04:32:09 +0200

    fix module name and python3 compatibility

--- test/serial_test.py
+++ test/serial_test.py
@@ -1,24 +1,29 @@
 import serial,time
-import RPi.GPIO as GPIO
+import NPi.GPIO as GPIO
 
+try:
+    input = raw_input
+except NameError:
+    pass
+  
 ser = serial.Serial('/dev/ttyS1',115200,timeout = 1)
 #ser.open()
 
-print ser.portstr
+print(ser.portstr)
 
-strInput = raw_input("Enter some words:")
+strInput = input("Enter some words:")
 num = ser.write(strInput)
-print num
+print(num)
 
 time.sleep(1)
 str = ser.read(num)#ser.readall()
-print str
+print(str)
 ser.close()
 
 #try:
 #    while True:
 #        response = ser.readline()
-#        print response
+#        print(response)
 #except KeyboardInterrupt:
 #    ser.close()
 
