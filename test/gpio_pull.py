5c8ee3bc60e4616199338a1039fa5cb2b8a5189e
Tungsteno74 <tungsteno74@yahoo.it>
2018-10-05 04:10:40 +0200

    fix python3 compatibility

--- test/gpio_pull.py
+++ test/gpio_pull.py
@@ -4,14 +4,14 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-print "\n value_7 = %d\n" %(GPIO.input(7))	
+print("\n value_7 = %d\n" %(GPIO.input(7)))	
 
 GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-print "\n value_8 = %d\n" %(GPIO.input(8))	
+print("\n value_8 = %d\n" %(GPIO.input(8)))	
 
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-print "\n value_12 = %d\n" %(GPIO.input(12))	
+print("\n value_12 = %d\n" %(GPIO.input(12)))	
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-print "\n value_12 = %d\n" %(GPIO.input(12))	
+print("\n value_12 = %d\n" %(GPIO.input(12)))
 
 #GPIO.cleanup()
