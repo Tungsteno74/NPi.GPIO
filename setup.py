869cef924730b35441e6475cb0585fd6121e6f35
Tungsteno74 <tungsteno74@yahoo.it>
2019-05-25 23:18:27 +0200

    update version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-__version__ = '0.5.8.2'
+__version__ = '0.5.8.3'
 
 classifiers = """\
 Development Status :: 5 - Production/Stable
