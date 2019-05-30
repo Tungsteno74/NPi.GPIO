1b8cf8a2ac007891591c6700fcdb9d76f40be474
Tungsteno74 <tungsteno74@yahoo.it>
2019-05-30 21:20:16 +0200

    Update __init__.py

--- NPi/GPIO/__init__.py
+++ NPi/GPIO/__init__.py
@@ -1,11 +1,8 @@
+from NPi._GPIO import *
+
 def pkg_metadata (*args):
   from pkg_resources import get_distribution
   pkg = get_distribution(args[0])
   return getattr(pkg, args[1])
 
-
-# __variables__ with double-quoted values will be available in setup.py
 __version__ = pkg_metadata("NPi.GPIO", "version")
-
-from NPi._GPIO import *
-
